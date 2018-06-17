c8f44197fdc4b19cd0eaa675e4ea6a7ca7ee308a
Tim De Pauw <timdp@users.noreply.github.com>
2018-06-17 15:34:08 +0200

    Fix up demo

--- demo/common.js
+++ demo/common.js
@@ -1,4 +1,24 @@
 (() => {
+  const pad = len => num => {
+    num = '' + num
+    while (num.length < len) {
+      num = '0' + num
+    }
+    return num
+  }
+
+  const pad2 = pad(2)
+
+  const pad3 = pad(3)
+
+  const formatTime = date => {
+    const hms = [date.getHours(), date.getMinutes(), date.getSeconds()]
+      .map(pad2)
+      .join(':')
+    const ms = pad3(date.getMilliseconds())
+    return `${hms}.${ms}`
+  }
+
   window.setUpLog = node => {
     const logs = document.createElement('fieldset')
     const legend = document.createElement('legend')
@@ -15,9 +35,10 @@
     })
 
     node.addEventListener('log', ({ detail: { message } }) => {
+      const time = formatTime(new Date())
       console.log(`${node}: ${message}`)
       const div = document.createElement('div')
-      div.innerText = message
+      div.innerText = `[${time}] ${message}`
       logs.appendChild(div)
     })
   }
