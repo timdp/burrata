912cce1272bd831ad6a825d4b781c81ab2de62f4
Tim De Pauw <timdp@users.noreply.github.com>
2018-06-18 10:45:22 +0200

    Make sure we produce ES5

---
+++ scripts/assert-es.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const { parse } = require('acorn')
+const yargs = require('yargs')
+const fs = require('fs')
+
+const { _: filenames, v: ecmaVersion } = yargs
+  .number('v')
+  .default('v', 5)
+  .strict()
+  .parse()
+
+let failures = 0
+
+for (const filename of filenames) {
+  let code = null
+  try {
+    code = fs.readFileSync(filename, 'utf8')
+  } catch (err) {
+    console.error(`Failed to read ${filename}: ${err}`)
+    ++failures
+    continue
+  }
+  try {
+    parse(code, { ecmaVersion })
+  } catch (err) {
+    ++failures
+    console.error(
+      `Failed to parse ${filename} as ECMAScript ${ecmaVersion}: ${err}`
+    )
+  }
+}
+
+process.exit(failures)
--- webpack.babel.js
+++ webpack.babel.js
@@ -1,18 +1,31 @@
 const path = require('path')
-const { babel: BABEL_OPTS } = require('./package.json')
+const {
+  babel: BABEL_OPTS,
+  dependencies
+} = require('./package.json')
 
-const ES_PACKAGES = [
-  'p-defer'
-]
+// Treat all dependencies as ES2015+ (e.g., p-defer and event-target-shim)
+const ES_PACKAGES = Object.keys(dependencies)
+  .filter(name => !name.startsWith('@babel/'))
+
+const NODE_MODULES = path.join(__dirname, 'node_modules')
+
+const PACKAGE_ALIASES = {
+  'event-target-shim': // Main is .mjs but that breaks the build
+    path.join(NODE_MODULES, 'event-target-shim/dist/event-target-shim.js')
+}
 
 const babelRule = {
-  test: /\.js$/,
+  test: /\.m?js$/,
   include: [
     path.join(__dirname, 'src'),
     path.join(__dirname, 'test'),
-    ...(ES_PACKAGES.map(name => path.join(__dirname, 'node_modules', name)))
+    ...(ES_PACKAGES.map(name => path.join(NODE_MODULES, name)))
   ],
   exclude: [],
+  resolve: {
+    alias: PACKAGE_ALIASES
+  },
   use: {
     loader: 'babel-loader',
     options: BABEL_OPTS
